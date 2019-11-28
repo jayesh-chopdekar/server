c6f338a32e1c87656ea1d7ab7da2238c6e6456c9
demo1 <jayesh250260@gmail.com>
2019-11-28 17:31:36 +0530

    Kuch bhi

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const routerProduct = require('./routes/product')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(bodyParser.json())
+
+// use all the routers
+app.use('/product', routerProduct)
+
+// routes
+app.use('/', routerProduct)
+
+app.listen(9898, '0.0.0.0', () => {
+    console.log('server started on port 9898')
+})
